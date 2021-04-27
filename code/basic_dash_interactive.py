8105521e6043edf9595350ca62e4aaabc49095ba
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2021-04-27 11:59:07 +0200

    color fixes

--- code/basic_dash_interactive.py
+++ code/basic_dash_interactive.py
@@ -17,6 +17,8 @@ df = pd.read_csv("../data/Motor_Vehicle_Collisions_-_Crashes.csv",low_memory=Fal
 df['CRASH DATE'] = pd.to_datetime(df['CRASH DATE'])
 df['CRASH TIME'] = pd.to_datetime(df['CRASH TIME'])
 available_years = np.sort(df['CRASH DATE'].dt.year.unique())[:-1]
+radio_buttons = ['TOTAL COLLISIONS','NUMBER OF PERSONS INJURED','NUMBER OF PERSONS KILLED', 'NUMBER OF PEDESTRIANS INJURED',
+'NUMBER OF PEDESTRIANS KILLED','NUMBER OF CYCLIST INJURED','NUMBER OF CYCLIST KILLED','NUMBER OF MOTORIST INJURED','NUMBER OF MOTORIST KILLED']
 
 # Markdown text
 markdown_text = '''
@@ -34,14 +36,37 @@ controls = dbc.Card(
     [
         dbc.FormGroup(
             [
-                dbc.Label("Year"),
+                dbc.Label("Year of collision: "),
                 dcc.Dropdown(
-                    id='collision-year',
+                    id='collision-year-dropdown',
                     options=[{'label': i, 'value': i} for i in available_years],
                     value=2020
                 ),
             ]
         ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Choose which kind of data you want to see: "),
+                dbc.RadioItems(
+                    id="data-type-radio",
+                    options=[{'label': i, 'value': i} for i in radio_buttons],
+                    value='TOTAL COLLISIONS',
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label('Radius: '),
+                dcc.Slider(
+                    id="radius-slider", 
+                    min=1,
+                    max=20,
+                    step=1,
+                    value=1,
+                    marks={1:'1', 5:'5', 10:'10', 15:'15', 20:'20'}
+                    )
+            ]
+        )
     ],
     body = True, 
     #color='dark'
@@ -56,12 +81,20 @@ def bar_borough_collisions():
 # This method takes input from the dropdown and spits out a figure, we use callback for this scenario
 @app.callback(
     Output('heatmap-graphic', 'figure'),
-    Input('collision-year', 'value'))
-def update_graph(collision_year):
+    Input('collision-year-dropdown', 'value'),
+    Input('data-type-radio', 'value'),
+    Input('radius-slider', 'value'))
+def update_graph(collision_year, data_type, radius_val):
     dff = df[df['CRASH DATE'].dt.year==collision_year]
-    fig = px.density_mapbox(dff, lat='LATITUDE', lon='LONGITUDE', radius=1,
-                        center=dict(lat=40.730610, lon=-73.935242), zoom=10,
-                        mapbox_style="stamen-terrain",height=800)
+    fig = None 
+    if (data_type == 'TOTAL COLLISIONS'): 
+        fig = px.density_mapbox(dff, lat='LATITUDE', lon='LONGITUDE', radius=radius_val,
+                            center=dict(lat=40.730610, lon=-73.935242), zoom=10,
+                            mapbox_style="carto-positron",height=800)
+    else: 
+        fig = px.density_mapbox(dff, lat='LATITUDE', lon='LONGITUDE', z=data_type, radius=radius_val,
+                            center=dict(lat=40.730610, lon=-73.935242), zoom=10,
+                            mapbox_style="carto-positron",height=800)
     return fig
 
 # We assemble all the parts of the website here
@@ -80,9 +113,10 @@ app.layout = dbc.Container(
         dcc.Markdown(children=markdown_text),
         dbc.Row(
             [
-                dbc.Col(controls, md=4),
+                dbc.Col(controls, md=3),
                 dbc.Col(dcc.Graph(id="heatmap-graphic"), md=8),
-            ]
+            ],
+            align='center'
         ),
     ],
     fluid = True,
